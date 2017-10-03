df29aad37313e64c9b6e67b47f42cd97b1656d02
Adam S. Jermyn <asj42@ast.cam.ac.uk>
2017-10-03 12:02:45 +0100

    Updated README and initial commit of code.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ADS-suggestions
 This script uses the API for NASA's ADS to suggest references related to those listed in a bibtex file.
+
+It takes as input the path to the bibtex file and the number of related references to produce.
+Related references are sorted by the number of times they appear either referencing or being referenced by papers in the bibtex file.
+The output is printed out with one reference per line.
+Each line begins with the number of occurences in the citation graph, the title of the paper and the author list.
+
+Note that this package requires the [bibtexparser](https://bibtexparser.readthedocs.io/en/v0.6.2/) and [ads](https://ads.readthedocs.io/en/latest/) packages.
+It also requires that you create a file in your home directory called `adstoken' containing your NASA ADS API token.
+If you don't have a token you can obtain one here(https://github.com/adsabs/adsabs-dev-api).
---
+++ refs.py
@@ -0,0 +1,38 @@
+import sys
+from os.path import expanduser
+home = expanduser("~")
+
+fname = sys.argv[1]
+num = int(sys.argv[2])
+
+import ads
+fi = open(home + '/adstoken','r')
+line = fi.readline().strip()
+ads.config.token = line
+fi.close()
+
+import bibtexparser
+with open(fname) as bibtex_file:
+    bibtex_str = bibtex_file.read()
+
+bib_database = bibtexparser.loads(bibtex_str)
+
+from collections import Counter
+
+papers = list(list(ads.SearchQuery(bibcode=b[u'ID'], fl=['citation', 'reference'])) for b in bib_database.entries)
+papers = list(p[0] for p in papers if len(p) > 0)
+
+refCounter = Counter()
+
+for p in papers:
+	refCounter.update(p.reference)
+	refCounter.update(p.citation)
+
+for b in bib_database.entries:
+	del refCounter[b[u'ID']]
+
+for c in refCounter.most_common(num):
+	if c[1] > 1:
+		p = list(ads.SearchQuery(bibcode=c[0], fl=['title','author']))[0]
+		print(c[1], p.title, p.author)
+
